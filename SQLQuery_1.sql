e3cb08c4eefe0b5b3e20de12256d7e5fc687b734
jeffercs96 <jeffercs96@gmail.com>
2023-11-15 23:21:30 -0500

    update query

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -1,1 +1,7 @@
 CREATE DATABASE classroomdb;
+
+CREATE TABLE Archivos (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Descripcion NVARCHAR(255),
+    Ruta NVARCHAR(1024) -- Ajusta la longitud según tus necesidades
+);
